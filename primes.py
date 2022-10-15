31e7a3e3ff877fa9bf03e5161372e988750c8ca7
wanjw270 <114835721+wanjw270@users.noreply.github.com>
2022-10-16 00:48:21 +0100

    initial commit

--- primes.py
+++ primes.py
@@ -3,4 +3,15 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+    count = 0
+    while count < number_of_primes:
+        isPrime = True
+        for i in range(2, num):
+            if num % i == 0:
+                isPrime = False
+        if isPrime:
+            list.append(num)
+        num += 1   
+        count += 1 
     return list
